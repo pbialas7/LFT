86818ea54a96867e29ba1be24bbfe184ea3304e2
Piotr <pbialas7@gmail.com>
2025-06-09 17:20:23 +0200

    Sweep with frozen sites.

--- src/LFT/MonteCarlo/sweep.h
+++ src/LFT/MonteCarlo/sweep.h
@@ -19,3 +19,23 @@ typename F::lattice_t::size_t sweep(F &field, U update) {
 
     return accepted;
 }
+
+
+template<typename F, typename U>
+typename F::lattice_t::size_t sweep(F &field, U update, std::vector<typename F::lattice_t::size_t> &fixed_sites) {
+    auto lat = field.lat;
+    typename F::lattice_t::size_t accepted = 0;
+    for (auto i = 0; i < lat.n_elements / 2; ++i) {
+        auto site = lat.even(i);
+        if (!std::binary_search(fixed_sites.begin(), fixed_sites.end(), site))
+            accepted += update(field, site);
+    }
+
+    for (auto i = 0; i < lat.n_elements / 2; ++i) {
+        auto site = lat.odd(i);
+        if (!std::binary_search(fixed_sites.begin(), fixed_sites.end(), site))
+        accepted += update(field, site);
+    }
+
+    return accepted;
+}
