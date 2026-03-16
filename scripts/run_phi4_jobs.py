8fb6f95d42c1d391e7f6c90dda6cab0af2467ed1
Piotr <pbialas7@gmail.com>
2026-03-16 11:17:06 +0100

    Updating .idea files.

--- scripts/run_phi4_jobs.py
+++ scripts/run_phi4_jobs.py
@@ -2,15 +2,19 @@ import subprocess as proc
 import time
 import numpy as np
 
-L = 16
+L = 8
+m2 = -4
 
-for m2 in np.arange(1.15,1.25,0.01): 
-    m2i = int(10000 * m2)
+n_sweeps = 4000000
+
+for lamda in (27,):
+    lamda = float(lamda)
     start = time.time()
     results = proc.run(
-        ['./cmake-build-release/phi4', '--lambda', "1.0", '--kappa', "1.0", '--n-term', "1000", '--n-sweeps', "1000000",
+        ['./cmake-build-release/phi4', '--lambda', f"{lamda}", '--kappa', "1.0", '--n-term', "1000", '--n-sweeps',
+         f"{n_sweeps}",
          '--save-freq',
-         "10", '--seed', "34", '--Lx', f"{L}", '--Ly', f"{L}", '--M2', f"{-m2}", "--output",
-         f"data/k100_m{m2i:05d}_{L:02d}x{L:02d}.bin"])
+         "10", '--seed', "34", '--Lx', f"{L}", '--Ly', f"{L}", '--M2', f"{m2}", "--output",
+         f"phi4_data/cfgs_{L:02d}x{L:02d}_l{int(lamda * 100):05d}_m2m0400.bin"])
     end = time.time()
-    print(f"ellapsed time {end - start:.2f}s")
+    print(f"Ellapsed time {end - start:.2f}s")
