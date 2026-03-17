a71d196138ba938fc730db9bca19834b9b1bc331
Piotr <pbialas7@gmail.com>
2026-03-17 18:31:52 +0100

    Fixed a efficiency problem with call to sweep. update parameter was passed by value instead of reference.

--- src/LFT/MonteCarlo/sweep.h
+++ src/LFT/MonteCarlo/sweep.h
@@ -9,7 +9,7 @@
 #include <omp.h>
 
 template<typename F, typename U, typename RNG>
-F::lattice_t::size_t sweep(F &field, U update) {
+F::lattice_t::size_t sweep(F &field, U& update) {
     auto lat = field.lat;
     typename F::lattice_t::size_t accepted = 0;
     for (size_t i = 0; i < lat.n_elements / 2; i++) {
@@ -26,7 +26,7 @@ F::lattice_t::size_t sweep(F &field, U update) {
 }
 
 template<typename F, typename U, typename RNG>
-F::lattice_t::size_t sweep_mt(F &field, U update, RNG &rng) {
+F::lattice_t::size_t sweep_mt(F &field, U& update, RNG &rng) {
     typename F::lattice_t::size_t accepted = 0;
 #pragma omp parallel for reduction(+:accepted) shared(update, rng)
     for (size_t i = 0; i < field.lat.n_elements / 2; i++) {
@@ -41,25 +41,5 @@ F::lattice_t::size_t sweep_mt(F &field, U update, RNG &rng) {
         auto site = field.lat.odd(i);
         accepted += update(field, site, rng[t]);
     }
-    return accepted;
-}
-
-
-template<typename F, typename U>
-typename F::lattice_t::size_t sweep(F &field, U update, std::vector<typename F::lattice_t::size_t> &fixed_sites) {
-    auto lat = field.lat;
-    typename F::lattice_t::size_t accepted = 0;
-    for (auto i = 0; i < lat.n_elements / 2; ++i) {
-        auto site = lat.even(i);
-        if (!std::binary_search(fixed_sites.begin(), fixed_sites.end(), site))
-            accepted += update(field, site);
-    }
-
-    for (auto i = 0; i < lat.n_elements / 2; ++i) {
-        auto site = lat.odd(i);
-        if (!std::binary_search(fixed_sites.begin(), fixed_sites.end(), site))
-            accepted += update(field, site);
-    }
-
     return accepted;
 }
