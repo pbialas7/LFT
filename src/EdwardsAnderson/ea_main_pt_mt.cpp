7905878bef0021b77dbb10687c193137fd383573
pbialas7 <pbialas7@gmail.com>
2026-06-23 10:23:05 +0200

    Refactoring initialisation in EA model.

--- src/EdwardsAnderson/ea_main_pt_mt.cpp
+++ src/EdwardsAnderson/ea_main_pt_mt.cpp
@@ -42,12 +42,7 @@ inline std::string format_hms(double total_seconds) {
     return oss.str();
 }
 
-inline void print_progress_bar(
-    std::int64_t current_iteration, // 1-based preferred
-    std::int64_t final_iterations,
-    const std::chrono::high_resolution_clock::time_point &start_time,
-    int bar_width = 40) {
-    if (final_iterations <= 0) return;
+
 
 #include "utils/progress_bar.h"
 
